40bcd8a02195bce17173ed3ab55bf98e4f93e965
11AA2200 <151981750+11AA2200@users.noreply.github.com>
2023-11-24 22:24:26 -0500

    Update worldcupDB.h

--- worldcupDB.h
+++ worldcupDB.h
@@ -1,1 +1,31 @@
 
+#ifndef _WORLD_CUP_DB_H_
+#define _WORLD_CUP_DB_H_
+
+#define TEAM_NAME_LENGTH_LIMIT 255
+#define PLAYER_NAME_LENGTH_LIMIT 255
+#define PLAYER_CLUB_LENGTH_LIMIT 255
+
+typedef struct team {
+  int code;
+  char name[TEAM_NAME_LENGTH_LIMIT];
+  char seeding[3];
+  char primaryKit;
+  struct team* next;
+} team;
+
+typedef struct player {
+  int code;
+  char name[PLAYER_NAME_LENGTH_LIMIT];
+  int age;
+  char club[PLAYER_CLUB_LENGTH_LIMIT];
+  struct player* next;
+} player;
+
+team *createTeam(int code, char *name, char *seeding, char primaryKit);
+void printTeam(team *team);
+
+player *createPlayer(int code, char *name, int age, char *club);
+void printPlayer(player *player);
+
+#endif
