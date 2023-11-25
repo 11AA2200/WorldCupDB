efa7706b5484a2dbff9ec7364edacc92e93d2024
11AA2200 <151981750+11AA2200@users.noreply.github.com>
2023-11-24 22:23:15 -0500

    Update worldcup_team.h

--- worldcup_team.h
+++ worldcup_team.h
@@ -1,1 +1,7 @@
+#include "worldCupDB.h"
 
+void insertTeam(team** teamList, team* team);
+team* findTeamByCode(team** teamList, int code);
+void deleteTeamByCode(team** teamList, int code);
+void printTeams(team* teamList);
+void freeTeamsDb(team* teamList);
