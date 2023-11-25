d5b2caa7993f9bfc4d0ff300a7348560b1e1b3cd
11AA2200 <151981750+11AA2200@users.noreply.github.com>
2023-11-24 22:20:07 -0500

    Update worldcup_player.c

--- worldcup_player.c
+++ worldcup_player.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "worldcup_player.h"
 
+void insertPlayer(player **playerList, player *player) {
+	player->next = *playerList;
+	*playerList = player;	
+}
+
+player *findPlayerByCode(player **playerList, int code) {
+	player* curr = *playerList;
+	while(curr != NULL) {
+		if (curr->code == code) {
+			return curr;
+		}
+		curr = curr->next;
+	}
+	return NULL;
+}
+
+void deletePlayerByCode(player **playerList, int code) {
+	player* curr = *playerList;
+	if (curr->code == code) {
+		*playerList = (*playerList)->next;
+		free(curr);
+	}
+	else {
+		while(curr->next != NULL) {
+			if (curr->next->code == code) {
+				player* tmp = curr->next;
+				curr->next = curr->next->next;
+				free(tmp);
+			}
+			curr = curr->next;
+		}
+	}
+}
+
+void printPlayers(player *playerList) {
+	player* curr = playerList;
+	printf("Teams list\n");
+	while(curr != NULL) {
+		printPlayer(curr);
+		printf("\n");
+		curr = curr->next;
+	}
+}
+
+void freePlayersDb(player *playerList) {
+	player* curr = playerList;
+	while(curr != NULL) {
+		player* tmp = curr->next;
+		free(curr);
+		curr = tmp;
+	}
+}
