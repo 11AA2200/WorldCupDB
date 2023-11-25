89d3d32fe9e2151d5c5db6c565b696b7e9cef6fb
11AA2200 <151981750+11AA2200@users.noreply.github.com>
2023-11-24 22:22:37 -0500

    Update worldcup_team.c

--- worldcup_team.c
+++ worldcup_team.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "worldcup_team.h"
 
+void insertTeam(team** teamList, team* team) {
+	team->next = *teamList;
+	*teamList = team;
+}
+
+team* findTeamByCode(team** teamList, int code) {
+	team* curr = *teamList;
+	while(curr != NULL) {
+		if (curr->code == code) {
+			return curr;
+		}
+		curr = curr->next;
+	}
+	return NULL;
+}
+
+void deleteTeamByCode(team** teamList, int code) {
+	team* curr = *teamList;
+	if (curr->code == code) {
+		*teamList = (*teamList)->next;
+		free(curr);
+	}
+	else {
+		while(curr->next != NULL) {
+			if (curr->next->code == code) {
+				team* tmp = curr->next;
+				curr->next = curr->next->next;
+				free(tmp);
+			}
+			curr = curr->next;
+		}
+	}
+}
+
+void printTeams(team* teamList) {
+	team* curr = teamList;
+	printf("Teams list\n");
+	while(curr != NULL) {
+		printTeam(curr);
+		printf("\n");
+		curr = curr->next;
+	}
+}
+
+void freeTeamsDb(team* teamList) {
+	team* curr = teamList;
+	while(curr != NULL) {
+		team* tmp = curr->next;
+		free(curr);
+		curr = tmp;
+	}
+}
